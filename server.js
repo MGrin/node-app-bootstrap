81ce034409182d61e33c0594f5d67fc47b5da62d
MGrin <mr6r1n@gmail.com>
2016-09-23 21:22:06 +0200

    some modifs

--- server.js
+++ server.js
@@ -1,10 +1,4 @@
 "use strict";
+require('babel-register');
 
-import express from "express";
-
-const app = express();
-app.use('/', express.static('public'));
-app.listen(process.env.PORT || 3000, (err) => {
-  console.log("Server started on port 3000");
-  
-});
+require('./server');
